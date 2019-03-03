cf2f1bb822597496b648c93bda7f08470e37bbf5
Germano Costa Neto <germano.cneto@gmail.com>
2019-03-03 14:37:31 -0300

    Update FRsampling.R

--- R/FRsampling.R
+++ R/FRsampling.R
@@ -36,14 +36,13 @@ FR.sampling = function(df.y, f = .2 , part.env = 1,intercept=TRUE){
 
   out.p    = FR.model(df.y = Ycopy,intercept = intercept)
 
-  yHat     = data.frame(melt(predict.FR(out.p,cvp,intercept = intercept)),pop=NA)
+  yHat     = data.frame(melt(predict.FR(out.p,cvp,intercept = intercept)))
   names(yHat)  = c("env","gid","yHat")
-  yHat = merge(yHat,Y,by=c("env","gid"))
-  yHat$pop[( yHat$env %in% tenv)  &   (yHat$gid %in% tgid)]   = "New GxL"
-  yHat$pop[ (yHat$env %in% tenv)  &  !(yHat$gid %in% tgid)]   = "New L|Know G"
-  yHat$pop[!(yHat$env %in% tenv)  &   (yHat$gid %in% tgid)]   = "Know L|New G"
-  yHat$pop[!( yHat$env %in% tenv) &  !(yHat$gid %in% tgid)]   = "Know GxL"
-
+  yHat = data.frame(merge(yHat,Y,by=c("env","gid")),pop=NA)
+  yHat$pop[( yHat$env %in% tenv )  &   (yHat$gid %in% tgid)]   = "New MET"
+  yHat$pop[ (yHat$env %in% tenv )  &  !(yHat$gid %in% tgid)]   = "New Location"
+  yHat$pop[!(yHat$env %in% tenv )  &   (yHat$gid %in% tgid)]   = "New Genotype"
+  yHat$pop[!( yHat$env %in% tenv)  &  !(yHat$gid %in% tgid)]   = "Within MET"
 
 
   return(list(yHat=yHat,sum.of.squares=out.p$'sum.of.squares',coefficients=out.p$'coefficients',frac.ss=out.p$"frac.ss"))
